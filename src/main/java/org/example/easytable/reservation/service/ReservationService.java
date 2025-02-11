b3f9e5c8c805d4b6cca8f8d6fb2a324547cded76
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-02-11 20:31:10 +0900

    feat : add exception 예외처리 완료함

--- src/main/java/org/example/easytable/reservation/service/ReservationService.java
+++ src/main/java/org/example/easytable/reservation/service/ReservationService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.example.easytable.exception.CustomException;
+import org.example.easytable.exception.ErrorCode;
 import org.example.easytable.member.entity.Member;
 import org.example.easytable.reservation.dto.response.ReservationCreateResDto;
 import org.example.easytable.reservation.dto.response.ReservationGetResDto;
@@ -27,7 +29,7 @@ public class ReservationService {
             LocalDateTime reservationTime) {
 
         Restaurant foundRestaurant = restaurantRepository.findById(restaurantId)
-                .orElseThrow(() -> new RuntimeException());
+                .orElseThrow(() -> CustomException.of(ErrorCode.NOT_FOUND, "존재하지 않는 식당입니다"));
 
         Reservation createdReservation = Reservation.builder()
                 .member(new Member()) //member 코드 추가시에 member 지정 가능
@@ -57,10 +59,11 @@ public class ReservationService {
                         reservation.getStatus()))
                 .collect(Collectors.toList());
     }
+
     @Transactional
     public void deleteReservation(Long restaurantId, Long reservationId) {
         Reservation foundReservation = reservationRepository.findById(reservationId)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 예약입니다"));
+                .orElseThrow(() -> CustomException.of(ErrorCode.NOT_FOUND,"존재하지 않는 예약입니다"));
 
         if (!foundReservation.getRestaurant().getId().equals(restaurantId)) {
             throw new RuntimeException("이 예약은 해당 식당에 속하지 않습니다.");
