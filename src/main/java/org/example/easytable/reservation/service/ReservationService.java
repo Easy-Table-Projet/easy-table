02a70f5b67f9445c0169db852dc9cd37682be526
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-02-11 19:53:24 +0900

    feat : add restaurant codes 레스토랑 코드 추가해서 적절하게 수정함

--- src/main/java/org/example/easytable/reservation/service/ReservationService.java
+++ src/main/java/org/example/easytable/reservation/service/ReservationService.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.example.easytable.member.entity.Member;
-import org.example.easytable.reservation.dto.response.ReservationCreateRes;
-import org.example.easytable.reservation.dto.response.ReservationGetRes;
+import org.example.easytable.reservation.dto.response.ReservationCreateResDto;
+import org.example.easytable.reservation.dto.response.ReservationGetResDto;
 import org.example.easytable.reservation.entity.Reservation;
 import org.example.easytable.reservation.entity.ReservationStatus;
 import org.example.easytable.reservation.repository.ReservationRepository;
 import org.example.easytable.restaurant.entity.Restaurant;
+import org.example.easytable.restaurant.repository.RestaurantRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,15 +20,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final RestaurantRepository restaurantRepository;
 
     @Transactional
-    public ReservationCreateRes save(Long restaurantId,
+    public ReservationCreateResDto save(Long restaurantId,
             LocalDateTime reservationTime) {
-        //TODO : RestaurantId로 식당 조회 및 예외처리
+
+        Restaurant foundRestaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new RuntimeException());
 
         Reservation createdReservation = Reservation.builder()
-                .member(new Member())
-                .restaurant(new Restaurant())
+                .member(new Member()) //member 코드 추가시에 member 지정 가능
+                .restaurant(foundRestaurant)
                 .reservationTime(reservationTime)
                 .status(ReservationStatus.CONFIRMED)
                 .isDeleted(false)
@@ -35,18 +39,18 @@ public class ReservationService {
 
         reservationRepository.save(createdReservation);
 
-        return new ReservationCreateRes(
+        return new ReservationCreateResDto(
                 createdReservation.getId(), createdReservation.getMember().getId(),
                 createdReservation.getRestaurant().getId(), createdReservation.getReservationTime(),
                 createdReservation.getStatus());
     }
 
 
-    public List<ReservationGetRes> getReservation() {
+    public List<ReservationGetResDto> getReservation() {
 
         List<Reservation> reservationList = reservationRepository.findAll();
 
-        return reservationList.stream().map(reservation -> new ReservationGetRes(
+        return reservationList.stream().map(reservation -> new ReservationGetResDto(
                         reservation.getMember().getId(),
                         reservation.getRestaurant().getId(),
                         reservation.getReservationTime(),
