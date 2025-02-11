f50109f526bb849b63e001780eebdd3b08182f7c
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-02-11 14:26:00 +0900

    feat : add save method 예약 등록 메서드 구현

--- src/main/java/org/example/easytable/reservation/controller/ReservationController.java
+++ src/main/java/org/example/easytable/reservation/controller/ReservationController.java
@@ -1,5 +1,34 @@
 package org.example.easytable.reservation.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.easytable.reservation.dto.request.ReservationCreateReq;
+import org.example.easytable.reservation.dto.response.ReservationCreateRes;
+import org.example.easytable.reservation.service.ReservationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/restaurants")
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @PostMapping("{restaurantId}/reservation")
+    public ResponseEntity<ReservationCreateRes> save(
+            @PathVariable("restaurantId") Long id,
+            ReservationCreateReq request
+    ) {
+
+        reservationService.save();
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
 
 }
