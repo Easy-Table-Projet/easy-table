aaaf8bf3abd85e9292f4a3f446b1d7924ff25e81
mikejigglypuff <k24555569@gmail.com>
2025-02-24 16:50:07 +0900

    comment: delete finished TODO

--- src/main/java/org/example/easytable/reservation/repository/ReservationQueueRepository.java
+++ src/main/java/org/example/easytable/reservation/repository/ReservationQueueRepository.java
@@ -5,7 +5,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ReservationQueueRepository {
-    // TODO: 인터페이스로 json 문자열 대신 DTO 객체를 받도록 수정할 것
     Mono<Boolean> addToWaitingQueue(String json, double score);
     Flux<String> getWaitingQueueRange(Range<Long> range);
     Mono<Long> removeFromWaitingQueue(String json);
